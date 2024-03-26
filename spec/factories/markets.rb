dfa6ce139d34372d801d115c2607935d6dd4f31a
Joey Reyes <joeyreyes105@gmail.com>
2024-03-25 17:23:09 -0700

    Complete index page for us-1, no vender count attribute added

---
+++ spec/factories/markets.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :market do
+    name { Faker::FunnyName.name }
+    street { Faker::Address.street_address }
+    city { Faker::Address.city }
+    county { Faker::Address.country }
+    state { Faker::Address.state }
+    zip { Faker::Address.zip }
+    lat { Faker::Address.longitude }
+    lon { Faker::Address.latitude }
+  end
+end
---
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+describe "Markets Api" do 
+    it "sends a list of markets" do 
+        create_list(:market, 4)
+
+        get "/api/v0/markets"
+
+        expect(response).to be_successful
+
+        markets = JSON.parse(response.body, symbolize_names: true)
+
+        expect(markets.count).to eq(4)
+        
+        markets.each do |market|
+            expect(market).to have_key(:id)
+            expect(market[:id]).to be_an(Integer)
+
+            expect(market).to have_key(:name)
+            expect(market[:name]).to be_a(String)
+
+            expect(market).to have_key(:street)
+            expect(market[:street]).to be_a(String)
+
+            expect(market).to have_key(:city)
+            expect(market[:city]).to be_a(String)
+
+            expect(market).to have_key(:county)
+            expect(market[:county]).to be_a(String)
+
+            expect(market).to have_key(:state)
+            expect(market[:state]).to be_an(String)
+
+            expect(market).to have_key(:zip)
+            expect(market[:zip]).to be_an(String)
+
+            expect(market).to have_key(:lat)
+            expect(market[:lat]).to be_an(String)
+
+            expect(market).to have_key(:lon)
+            expect(market[:lon]).to be_an(String)
+        end
+    end
+end
